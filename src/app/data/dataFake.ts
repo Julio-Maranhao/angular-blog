fa255dfb764923dc9e6651b83a1e0ed55ac9a60c
Julio Maranhao <julio.maranhao@outlook.com>
2023-04-19 12:21:33 -0300

    Menu bar layout finished

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,94 @@
+export const dataFake = [
+	{
+		'id':1,
+		'photo': 'https://img.freepik.com/fotos-gratis/foto-de-grande-angular-de-uma-unica-arvore-crescendo-sob-um-ceu-nublado-durante-um-por-do-sol-cercado-por-grama_181624-22807.jpg',
+		'title': 'Sed elit ipsum, congue eget',
+		'description': `Nunc egestas ultricies interdum. Aliquam orci quam, ultrices at purus non, gravida auctor lectus. Aenean a est lobortis, posuere sapien nec, auctor ligula. Donec diam metus, dictum sit amet diam vel, accumsan tincidunt velit. Nulla luctus finibus turpis a dignissim. Pellentesque porttitor vulputate facilisis. Fusce fermentum, mi id finibus interdum, dui dui efficitur tortor, sit amet fringilla justo mi et ante. Fusce ut imperdiet eros, eget porttitor eros. Sed condimentum, erat et sagittis tempor, turpis ex eleifend mauris, in semper eros neque at libero. Phasellus efficitur vehicula ligula, tincidunt ullamcorper mi bibendum nec. Pellentesque blandit id sem ac porttitor. Duis dictum lacinia porttitor. Vestibulum sit amet maximus mauris.
+
+		Pellentesque tempor nisl sit amet ligula porta ornare. Integer mauris velit, rutrum non tincidunt nec, blandit sed turpis. Curabitur purus lacus, placerat sit amet dolor sit amet, volutpat posuere massa. Maecenas vitae condimentum justo. Integer gravida augue dui, vel posuere enim cursus eget. Integer in feugiat velit. Quisque a cursus diam, ac congue mauris. Curabitur pulvinar placerat placerat. Pellentesque mauris nulla, ultrices vitae orci non, cursus molestie magna. In hac habitasse platea dictumst. Nulla facilisi. Donec scelerisque, nisi quis viverra semper, ligula lacus congue risus, ut tempus nisl est sed nisi. Nulla facilisis enim eu ligula congue laoreet. Fusce sed nibh ultricies, pretium lorem a, volutpat velit.`,
+		'date': 'november, 22, 2022',
+		'categories': ['campo', 'calmo'],
+	},
+	{
+		'id':2,
+		'photo': 'https://img.freepik.com/fotos-gratis/paisagem-de-nevoeiro-matinal-e-montanhas-com-baloes-de-ar-quente-ao-nascer-do-sol_335224-794.jpg',
+		'title': 'Duis in accumsan lectus',
+		'description': `Nullam est purus, tristique sit amet ultricies at, dignissim non sem. Morbi non arcu lacus. Pellentesque facilisis sapien eget sem ullamcorper cursus. Nulla facilisi. Pellentesque luctus, mi non convallis consectetur, erat sapien ullamcorper diam, ac feugiat velit tellus quis neque. Sed imperdiet venenatis ex, quis sodales ante dapibus vel. Cras nec purus sodales felis viverra commodo vel vel diam. Fusce lectus velit, eleifend a risus eu, cursus vulputate leo. Nam nisl est, posuere sit amet leo in, maximus auctor ipsum. Integer elementum nisl ultricies neque tempus interdum.
+
+		Fusce non efficitur orci. In eget neque tellus. Donec vestibulum elit et eros pharetra, vel lobortis massa ultrices. Suspendisse pellentesque nibh a lectus auctor gravida. Nam porta, augue in fringilla ultrices, sem metus lacinia ante, quis pulvinar velit nibh in ante. Maecenas dolor lectus, tempus vel arcu non, ullamcorper varius massa. Proin sapien ipsum, molestie vel leo ut, venenatis aliquam erat. Pellentesque sem nisi, facilisis eu libero vitae, laoreet ultricies felis. Duis quis enim quis turpis placerat varius. Sed eu neque augue. Vivamus lobortis urna in nunc sagittis, sollicitudin tempor tortor rhoncus. Vivamus quis malesuada nulla. Pellentesque enim velit, eleifend nec turpis eget, finibus interdum augue. Vestibulum scelerisque justo at mauris mattis fringilla. Vivamus non velit elit. Sed est ex, vestibulum mattis sagittis ullamcorper, facilisis ut purus.`,
+		'date': 'december, 20, 2022',
+		'categories': ['campo', 'aventura'],
+	},
+	{
+		'id':3,
+		'photo': 'https://img.freepik.com/fotos-gratis/a-paisagem-bonita-do-sol-da-praia-com-um-barco_1112-212.jpg',
+		'title': 'Maecenas facilisis vel odio sed suscipit',
+		'description': `
+		Proin tristique tellus eu mi fermentum, vitae finibus est imperdiet. Duis sit amet leo dignissim, lobortis leo ut, elementum justo. Nunc ut purus sed tellus faucibus consequat sit amet et felis. Duis condimentum odio non placerat accumsan. Cras efficitur convallis justo, at elementum sapien pulvinar quis. Quisque condimentum euismod tempus. Proin in erat sollicitudin, volutpat ligula vel, consectetur sem. Nullam interdum mauris et consectetur vehicula. Sed ipsum libero, elementum sed gravida vel, convallis id augue. In id dictum dui, a dictum augue. Morbi dignissim, ante in malesuada imperdiet, odio mi sagittis eros, id dictum ligula dui ut urna. Duis mollis ante sed interdum egestas. Cras erat augue, tincidunt ac odio id, volutpat dapibus neque. Donec sed nulla vitae leo sagittis faucibus. Morbi finibus felis leo, eget pharetra nunc porttitor vel. Vivamus quis feugiat libero, sed volutpat augue.
+
+		Nullam auctor mauris et ornare varius. Nam vel tortor leo. Vivamus quis nisl vitae ante congue lacinia sed in velit. Ut pretium id risus a aliquet. Ut euismod pulvinar risus, nec iaculis ex bibendum at. Nunc nec arcu quis tellus euismod suscipit. Suspendisse nisi libero, malesuada egestas augue sed, pharetra interdum orci. Quisque sollicitudin tellus felis, et placerat ligula rutrum et. Fusce ut purus eu ipsum rutrum dapibus. Mauris metus urna, sagittis vitae dapibus id, dapibus vel massa. Duis semper diam sit amet purus pretium sollicitudin. Aliquam vel fringilla magna, in molestie enim. Nunc consequat eros a dolor efficitur, id vulputate sapien finibus.`,
+		'date': 'january, 16, 2023',
+		'categories': ['praia', 'calmo'],
+	},
+	{
+		'id':4,
+		'photo': 'https://thumbs.dreamstime.com/b/paisagens-coloridas-do-cen%C3%A1rio-da-queda-46641435.jpg',
+		'title': 'Praesent gravida lacus at',
+		'description': `Proin nec nibh tortor. Curabitur sed imperdiet mauris. Etiam neque libero, convallis nec consequat at, rhoncus in justo. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Integer vitae posuere enim, scelerisque auctor leo. Sed faucibus dignissim nibh, et consequat elit semper non. Nullam fermentum porta tincidunt. Pellentesque laoreet sapien scelerisque, condimentum magna ut, sollicitudin est. Proin dignissim luctus scelerisque.
+
+		Integer tempus quam varius ex venenatis auctor. Ut sed diam vel felis dignissim dapibus. Cras eros mi, consectetur id urna lobortis, rutrum facilisis dui. Quisque quis hendrerit tortor, quis semper leo. Phasellus hendrerit sapien vitae lacus vestibulum efficitur et sit amet lacus. Ut eget felis pellentesque tellus aliquam hendrerit ac eu lectus. Cras sed bibendum justo, id gravida massa.`,
+		'date': 'february, 18, 2023',
+		'categories': ['campo', 'calmo'],
+	},
+	{
+		'id':5,
+		'photo': 'https://st.depositphotos.com/1000152/1336/i/600/depositphotos_13360440-stock-photo-tropical-sea.jpg',
+		'title': 'Aliquam vulputate, dui bibendum molestie fermentum',
+		'description': `Fusce suscipit ornare quam a feugiat. Etiam eleifend accumsan accumsan. Praesent tristique tincidunt erat, a blandit elit. Etiam mollis convallis ante et iaculis. Mauris iaculis blandit laoreet. Etiam aliquet, turpis at egestas laoreet, ante nulla molestie augue, quis facilisis dolor lorem tristique nibh. In aliquam, purus vel commodo bibendum, orci nisi venenatis nibh, sit amet condimentum ante nulla ut velit. Proin urna velit, interdum cursus eros vitae, ultricies semper est. Praesent tristique vitae dolor vel ornare. Aliquam ex orci, accumsan sed quam malesuada, molestie fringilla sem.
+
+		Curabitur maximus nisi purus, quis porttitor elit molestie ac. Etiam et sagittis nibh, vel ornare libero. Morbi in eros iaculis, laoreet sapien at, pharetra mi. Phasellus dignissim, augue tristique sollicitudin rhoncus, turpis quam condimentum justo, non scelerisque neque quam vel mi. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean aliquam vitae nisl non imperdiet. Aliquam aliquet tortor ac scelerisque molestie. Suspendisse nibh augue, dapibus ut aliquet vel, rutrum in arcu. Phasellus consequat enim eu odio commodo, at venenatis diam convallis. Aenean auctor magna ac metus sodales hendrerit. Vestibulum ac velit a est volutpat maximus ut vel nulla. Mauris quis turpis sit amet nisi porttitor facilisis at eu purus. Duis posuere mattis arcu vitae ultrices.`,
+		'date': 'march, 20, 2023',
+		'categories': ['praia', 'aventura'],
+	},
+	{
+		'id':6,
+		'photo': 'https://www.queroviajarmais.com/wp-content/uploads/2020/08/da-nang-bay.jpg',
+		'title': 'Nunc feugiat quis odio nec mollis',
+		'description': `
+		Praesent imperdiet diam in imperdiet tristique. Ut vel blandit tellus, non consectetur augue. Nunc non mi nec ex tristique posuere. Ut dui orci, blandit vitae metus ut, pulvinar luctus arcu. Suspendisse porta sagittis ipsum, sit amet ullamcorper lectus eleifend venenatis. Cras purus nibh, efficitur sed elementum eget, molestie vitae sem. Integer et porttitor velit. Aliquam nec mollis elit. Ut cursus nisl velit, ut semper justo dignissim et. Mauris mauris nisl, euismod eget aliquam sed, consectetur non risus. Nulla consectetur vestibulum lacus in tincidunt. Vestibulum mattis libero et arcu tempor luctus. Integer vitae lacus sit amet diam tempor finibus vel sit amet arcu. Maecenas quis justo quis lectus vehicula auctor. Donec et felis vestibulum, pellentesque urna vitae, pulvinar arcu.
+
+		Phasellus suscipit elit et aliquam sollicitudin. Donec pretium dignissim viverra. Phasellus tincidunt tincidunt libero eu interdum. Pellentesque maximus erat sed tellus placerat facilisis. Duis lobortis eu dui ac condimentum. Vestibulum elementum ac lorem lobortis dictum. Sed vitae pellentesque odio, vitae suscipit sem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae posuere risus. Maecenas in orci malesuada mi finibus vulputate. Mauris quis luctus mauris. In pellentesque pretium mi, non vestibulum tellus ullamcorper in. Cras faucibus urna non porttitor lacinia.`,
+		'date': 'april, 25, 2023',
+		'categories': ['praia', 'calmo'],
+	},
+	{
+		'id':7,
+		'photo': 'https://www.queroviajarmais.com/wp-content/uploads/2020/08/lago-louise.jpg',
+		'title': 'Nulla neque nisi, accumsan ac tortor',
+		'description': `Integer ut pharetra tortor. Fusce aliquam erat eget massa sodales feugiat. Pellentesque turpis felis, lacinia a lacinia et, finibus nec eros. Phasellus pellentesque id nunc at tincidunt. Suspendisse sit amet hendrerit urna, dictum sagittis libero. Vestibulum facilisis aliquet feugiat. Praesent gravida fermentum vulputate. Duis accumsan ultricies purus.
+
+		Maecenas sit amet ligula ipsum. Mauris auctor suscipit aliquet. Donec a auctor purus, sit amet faucibus orci. Cras sagittis pharetra mi, vitae venenatis augue molestie sed. Vivamus interdum tortor a fringilla mattis. Phasellus eu dictum turpis. Donec vitae ex eget enim pulvinar tempor. Mauris condimentum aliquam nisl id malesuada. Nulla et sem quis risus condimentum lobortis eget eget dolor. Etiam ex velit, porttitor eget laoreet vel, gravida non risus. Sed vel ligula purus. Duis congue, ligula ut mattis porttitor, nibh ligula vehicula nibh, quis viverra ante sapien dapibus orci.`,
+		'date': 'may, 22, 2023',
+		'categories': ['campo', 'aventura'],
+	},
+	{
+		'id':8,
+		'photo': 'https://i0.wp.com/omeudiadia.com.br/wp-content/uploads/2022/04/paisagem-natural.jpg?resize=800%2C534&ssl=1',
+		'title': 'Donec viverra eu tellus',
+		'description': `Duis sed dolor ullamcorper, sollicitudin orci quis, suscipit lacus. Mauris sollicitudin scelerisque arcu, id congue dui dapibus in. Morbi sodales commodo mi non elementum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Donec tempus nec dolor at suscipit. Suspendisse tempor leo at ultrices sodales. Nunc auctor felis ligula, vel bibendum lectus lacinia id. Curabitur placerat semper leo et consequat. Nam et orci at quam rhoncus malesuada a quis nisl. Sed pretium, metus et pulvinar sodales, dolor dui volutpat magna, sed accumsan magna enim nec ligula. Donec aliquet ut sapien non commodo.
+
+		Ut vel neque est. Quisque lobortis nec metus nec luctus. Nunc sit amet viverra erat. In commodo congue feugiat. Aliquam vehicula eros quis ultrices scelerisque. Pellentesque fermentum fermentum nisi. Curabitur non orci ac tellus semper cursus et eu nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;`,
+		'date': 'june, 24, 2023',
+		'categories': ['praia', 'calmo'],
+	},
+	{
+		'id':9,
+		'photo': 'https://blog.emania.com.br/wp-content/uploads/2015/12/paisagem-tropical-wallpaper-1.jpg',
+		'title': 'Aliquam sit amet augue odio. Donec',
+		'description': `Proin porttitor erat vel laoreet accumsan. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam ut massa dignissim, volutpat magna ac, tincidunt tortor. Mauris sodales eros est, quis tincidunt libero tristique nec. Aliquam erat volutpat. Maecenas ultrices dolor finibus, volutpat magna vitae, convallis dui. Curabitur sed lorem nibh. Vestibulum blandit, ipsum in pharetra scelerisque, libero leo mollis metus, nec rutrum dolor velit eu mauris. Vivamus porttitor elit non ligula tempus tincidunt. Fusce pretium, metus ut condimentum dictum, augue nibh faucibus nibh, ac dignissim purus ligula mattis orci. Donec eget sollicitudin sapien, ut finibus mauris. Mauris interdum aliquet mauris porttitor commodo.
+
+		Suspendisse in commodo nibh, eu lacinia nunc. Suspendisse tempus enim sit amet dui finibus, faucibus vulputate neque posuere. Aenean auctor accumsan eros ut consequat. Curabitur mollis tempus neque, at ultrices metus porttitor id. Donec eget finibus ligula. In a tempor quam, eu consectetur metus. Aenean pretium est nec dignissim lobortis. Aliquam commodo lacus at mauris vehicula, at posuere odio convallis. Ut quis consectetur ipsum. Nulla facilisi.`,
+		'date': 'july, 27, 2023',
+		'categories': ['praia', 'aventura'],
+	},
+]
